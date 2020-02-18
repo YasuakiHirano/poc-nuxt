2ef27714e9f3c85dcef626c719c4112b77552f13
y.hirano <yasuakihappy@gmail.com>
2020-02-18 23:10:06 +0900

    first comit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template :align="start" :justify="start">
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title class="headline">
+            Hello, World!
+          </v-card-title>
+          <v-card-text>
+            Nuxt+Vuetifyのテストページです
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+       import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+  export default {
+      components: {}
+  }
+</script>
