682eb84c93f8a49476e50d4b323e12c349f85ef9
y.hirano <yasuakihappy@gmail.com>
2020-05-05 01:18:45 +0900

    typescript  todo list

--- pages/typescript-test.vue
+++ pages/typescript-test.vue
@@ -1,33 +1,55 @@
 <template lang="pug">
   div.container
     v-row
+      v-col.just(cols="12")
+        span {{title}}
       v-col.just(cols="8")
-        v-text-field(v-model="textField" placeholder="input area")
-        v-btn
-          span(@click="testAction()") test!
+        v-text-field(v-model="todoItem")
+        v-btn.primary(@click="addTodo()") add
+    div
+      v-list
+        v-list-item-group(v-for="(item, key) in todos()")
+          v-list-item
+            v-checkbox
+            v-list-item-content {{item}}
+            v-list-item-icon
+              v-icon(@click="deleteTodo(key)") mdi-delete-circle
+          v-divider
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import { mapActions, mapGetters } from 'vuex'
 
 @Component({
   methods: {
-
+    ...mapGetters({
+      todos: 'todo/todoList'
+    }),
+    ...mapActions({
+      addTodoItem: 'todo/add',
+      deleteItem: 'todo/delete',
+    })
   }
 })
 export default class extends Vue {
-  private textField: string = '';
-
-  created() {
-    this.textField = 'test123';
-  }
+  private todoItem: string = '';
+  private title: string = '';
+  private todos!: () => [];
+  private addTodoItem!: (item: string) => Promise<void>;
+  private deleteItem!: (key: string) => Promise<void>;
 
   mounted() {
-    this.textField = 'test123';
+    this.title = 'typescriptでtodoリスト';
   }
 
-  public testAction() {
-    alert(this.textField);
+  public addTodo() {
+    this.addTodoItem(this.todoItem);
+    this.todoItem = '';
+  }
+
+  public deleteTodo(key: string) {
+    this.deleteItem(key);
   }
 }
 </script>
